0a0392a62742f7fe486557284979ba87902ad05b
Костя Симончик <simonchik2210@gmail.com>
2022-11-12 20:01:07 +0100

    first commit

---
+++ swiftbook_HW7/swiftbook_HW7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swiftbook_HW7
+//
+//  Created by Костя Симончик on 12.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //Some code
+    }
+
+
+}
+
