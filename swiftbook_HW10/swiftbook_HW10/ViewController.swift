92d0e6b0306cb5a2221629f5a0f89acb1c75b52c
Костя Симончик <simonchik2210@gmail.com>
2022-11-12 21:48:48 +0100

    swiftbook_HW10

---
+++ swiftbook_HW10/swiftbook_HW10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swiftbook_HW10
+//
+//  Created by Костя Симончик on 12.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
